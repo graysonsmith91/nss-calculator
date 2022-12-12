5b06d46fd8ba251375b9257ebb9f0e064e772294
Grayson Smith <graysonsmith91@gmail.com>
2022-12-12 09:52:38 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,62 @@
+// Function for addition.
+
+const add = (firstNumber, secondNumber) => {
+    const sum = firstNumber + secondNumber
+    return sum
+}
+
+let result = add()
+
+console.log(result)
+
+
+
+// Function for subtraction.
+
+const subtract = (firstNumber, secondNumber) => {
+    const difference = firstNumber - secondNumber
+    return difference
+}
+
+result = subtract()
+
+console.log(result)
+
+
+
+// Function for multiplication.
+
+const multiply = (firstNumber, secondNumber) => {
+    const product = firstNumber * secondNumber
+    return product
+}
+
+result = multiply()
+
+console.log(result)
+
+
+
+// Function for division.
+
+const divide = (firstNumber, secondNumber) => {
+    const quotient = firstNumber / secondNumber
+    return quotient
+}
+
+result = divide()
+
+console.log(result)
+
+
+
+// Function for square root.
+
+const square = (firstNumber) => {
+    const squared = firstNumber * firstNumber
+    return squared
+}
+
+result = square()
+
+console.log(result)
